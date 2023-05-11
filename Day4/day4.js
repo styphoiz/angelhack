632d634d2a806cd7c5c61f0631d4215d87c08309
Mohammed Hoosain <styphoiz@gmail.com>
2023-05-11 14:04:50 +0200

    Checked which one is left out

--- Day4/day4.js
+++ Day4/day4.js
@@ -1,36 +1,33 @@
-//Manually removed 335 because of the highest difference between it's neighbors.
-let arr = [1, 3, 54, 712, 52, 904, 113, 12, 135, 32, 31, 56, 23, 79, 611, 123, 677, 232, 997, 101, 111,
-123, 2, 7, 24, 57, 99, 45, 666, 42, 104, 129, 554, 876, 35, 15, 93, 13]
+const arr = [
+  1, 3, 54, 712, 52, 904, 113, 12, 135, 32, 31, 56, 23, 79, 611, 123, 677, 232,
+  997, 101, 111, 123, 2, 7, 24, 57, 99, 45, 666, 42, 104, 129, 554, 335, 876,
+  35, 15, 93, 13,
+];
+const sortedArr = [...arr]; // Copy the array to avoid modifying the original
 
-arr.sort((a,b) => a-b)
-console.log(arr)
-let sum = 0
-for (let i=0;i<arr.length-1;i=i+2){
-    sum += arr[i+1]-arr[i]
-    console.log("Difference",arr[i],arr[i+1]-arr[i], sum)
-}
-
-console.log(sum)
-const arr = [1, 3, 54, 712, 52, 904, 113, 12, 135, 32, 31, 56, 23, 79, 611, 123, 677, 232, 997, 101, 111, 123, 2, 7, 24, 57, 99, 45, 666, 42, 104, 129, 554, 335, 876, 35, 15, 93, 13];
-const sortedArr = [...arr];  // Copy the array to avoid modifying the original
-
-sortedArr.sort((a, b) => a - b);  // Sort the array in ascending order
+sortedArr.sort((a, b) => a - b); // Sort the array in ascending order
 
 const newArray = [];
 for (let i = 0; i < sortedArr.length; i++) {
   const currentValue = sortedArr[i];
   const filteredArray = sortedArr.filter((value, index) => index !== i);
   newArray.push(filteredArray);
+  let sum = 0;
+
+  for (let j = 0; j < filteredArray.length - 1; j = j + 2) {
+    sum += filteredArray[j + 1] - filteredArray[j];
+  }
+  console.log(sortedArr[i], sum);
 }
 
 //console.log(newArray);
-let sumArr = []
-newArray.forEach((arr)=> {
-let sum = 0
-for (let i=0;i<arr.length-1;i=i+2){
-    sum += arr[i+1]-arr[i]
+let sumArr = [];
+newArray.forEach((arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length - 1; i = i + 2) {
+    sum += arr[i + 1] - arr[i];
     //console.log("Difference",arr[i],arr[i+1]-arr[i], sum)
-}
-sumArr.push(sum)
-})
-console.log(sumArr.sort((a,b) => a-b))
+  }
+  sumArr.push(sum);
+});
+console.log(sumArr.sort((a, b) => a - b));
