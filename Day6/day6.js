93d3468aa77ab61ca8bcdc2f058281c9c211b173
Mohammed Hoosain <styphoiz@gmail.com>
2023-05-14 18:39:40 +0200

    Create day6.js

---
+++ Day6/day6.js
@@ -0,0 +1,54 @@
+function splitSeries(series) {
+
+  const splits = [];
+
+  let currentSplit = series[0];
+
+  for (let i = 1; i < series.length; i++) {
+
+    if (series[i] !== series[i - 1]) {
+
+      splits.push(currentSplit[0]);
+
+      currentSplit = "";
+
+    }
+
+    currentSplit += series[i];
+
+  }
+
+  splits.push(currentSplit[0]);
+
+  return splits;
+
+}
+
+// Example usage:
+
+const series = "kjslaqpwoereeeeewwwefifjdksjdfhjdksdjfkdfdlddkjdjfjfjfjjjjfjffnefhkjgefkgjefkjgkefjekihutrieruhigtefhgbjkkkknbmssdsdsfdvneurghiueor";
+
+const splitSeriesArray = splitSeries(series);
+
+console.log("Split series:", splitSeriesArray);
+
+let keys = {}
+
+for (let i=0;i<splitSeriesArray.length; i++){
+
+    if (keys[splitSeriesArray[i]]){
+
+        keys[splitSeriesArray[i]]++
+
+    }
+
+    else{
+
+        keys[splitSeriesArray[i]] = 1
+
+    }
+
+}
+
+console.log(keys)
+
