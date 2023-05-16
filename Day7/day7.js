37500014484e53689b871b481e753964f5f70e5c
Mohammed Hoosain <styphoiz@gmail.com>
2023-05-16 17:01:19 +0200

    Update day7.js

--- Day7/day7.js
+++ Day7/day7.js
@@ -1,6 +1,88 @@
-let a = [[".",".",".",".","X"],
+let array = [[".",".",".",".","X"],
 ["X",".",".","X","."],
 ["X",".",".","X","X"],
 [".",".","X",".","."],
 ["X",".",".",".","."]]
-console.log(a)
+
+let newArr = []
+for (let i = 0; i < array.length; i++) {
+
+  for (let j = 0; j < array[i].length; j++) {
+    let dot = 0
+    let x = 0
+    let adj = 0
+    const currentSquare = array[i][j];
+
+    console.log(`Current Square: ${currentSquare}`);
+
+    
+
+    // Check adjacent squares (top, bottom, left, right)
+
+    if (i > 0) {
+
+      const topSquare = array[i - 1][j];
+
+      console.log(`Top Square: ${topSquare}`);
+      if(topSquare == "."){
+          dot++
+      }else if (topSquare == "X"){
+          x++
+      }
+    adj++
+    }
+
+    if (i < array.length - 1) {
+
+      const bottomSquare = array[i + 1][j];
+
+      console.log(`Bottom Square: ${bottomSquare}`);
+if(bottomSquare == "."){
+          dot++
+      }else if (bottomSquare == "X"){
+          x++
+      }
+      adj++
+    }
+
+    if (j > 0) {
+
+      const leftSquare = array[i][j - 1];
+
+      console.log(`Left Square: ${leftSquare}`);
+if(leftSquare == "."){
+          dot++
+      }else if (leftSquare == "X"){
+          x++
+      }
+      adj++
+    }
+
+    if (j < array[i].length - 1) {
+
+      const rightSquare = array[i][j + 1];
+
+      console.log(`Right Square: ${rightSquare}`);
+if(rightSquare == "."){
+          dot++
+      }else if (rightSquare == "X"){
+          x++
+      }
+      adj++
+
+    }
+
+      console.log(dot)
+      console.log(x)    
+if(array[i] == "X"){
+    if(adj == dot){
+        newArr.push()
+    }
+}else if(array[i]= "."){
+    
+}
+    console.log('----------------------');
+
+  }
+
+}
