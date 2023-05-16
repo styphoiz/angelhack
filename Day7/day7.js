5e8982b848002988f1a493974cfa6b5747155534
Mohammed Hoosain <styphoiz@gmail.com>
2023-05-16 17:07:53 +0200

    Update day7.js

--- Day7/day7.js
+++ Day7/day7.js
@@ -1,21 +1,32 @@
-let array = [[".",".",".",".","X"],
-["X",".",".","X","."],
-["X",".",".","X","X"],
-[".",".","X",".","."],
-["X",".",".",".","."]]
+let array = [
 
-let newArr = []
+  [".", ".", ".", ".", "X"],
+
+  ["X", ".", ".", "X", "."],
+
+  ["X", ".", ".", "X", "X"],
+
+  [".", ".", "X", ".", "."],
+
+  ["X", ".", ".", ".", "."]
+
+];
+
+let newArr = [];
+
 for (let i = 0; i < array.length; i++) {
 
+  let newRow = [];
+
   for (let j = 0; j < array[i].length; j++) {
-    let dot = 0
-    let x = 0
-    let adj = 0
+
+    let dot = 0;
+
+    let x = 0;
+
+    let adj = 0;
+
     const currentSquare = array[i][j];
-
-    console.log(`Current Square: ${currentSquare}`);
-
-    
 
     // Check adjacent squares (top, bottom, left, right)
 
@@ -23,66 +34,106 @@ for (let i = 0; i < array.length; i++) {
 
       const topSquare = array[i - 1][j];
 
-      console.log(`Top Square: ${topSquare}`);
-      if(topSquare == "."){
-          dot++
-      }else if (topSquare == "X"){
-          x++
+      if (topSquare == ".") {
+
+        dot++;
+
+      } else if (topSquare == "X") {
+
+        x++;
+
       }
-    adj++
+
+      adj++;
+
     }
 
     if (i < array.length - 1) {
 
       const bottomSquare = array[i + 1][j];
 
-      console.log(`Bottom Square: ${bottomSquare}`);
-if(bottomSquare == "."){
-          dot++
-      }else if (bottomSquare == "X"){
-          x++
+      if (bottomSquare == ".") {
+
+        dot++;
+
+      } else if (bottomSquare == "X") {
+
+        x++;
+
       }
-      adj++
+
+      adj++;
+
     }
 
     if (j > 0) {
 
       const leftSquare = array[i][j - 1];
 
-      console.log(`Left Square: ${leftSquare}`);
-if(leftSquare == "."){
-          dot++
-      }else if (leftSquare == "X"){
-          x++
+      if (leftSquare == ".") {
+
+        dot++;
+
+      } else if (leftSquare == "X") {
+
+        x++;
+
       }
-      adj++
+
+      adj++;
+
     }
 
     if (j < array[i].length - 1) {
 
       const rightSquare = array[i][j + 1];
 
-      console.log(`Right Square: ${rightSquare}`);
-if(rightSquare == "."){
-          dot++
-      }else if (rightSquare == "X"){
-          x++
+      if (rightSquare == ".") {
+
+        dot++;
+
+      } else if (rightSquare == "X") {
+
+        x++;
+
       }
-      adj++
+
+      adj++;
 
     }
 
-      console.log(dot)
-      console.log(x)    
-if(array[i] == "X"){
-    if(adj == dot){
-        newArr.push()
+    if (currentSquare == "X") {
+
+      if (adj == dot) {
+
+        newRow.push(".");
+
+      } else {
+
+        newRow.push("X");
+
+      }
+
+    } else if (currentSquare == ".") {
+
+      if (x === 1 || x === 2) {
+
+        newRow.push("X");
+
+      } else {
+
+        newRow.push(".");
+
+      }
+
     }
-}else if(array[i]= "."){
-    
-}
-    console.log('----------------------');
 
   }
 
+  newArr.push(newRow);
+
 }
+
+console.log("New Array:");
+
+console.log(newArr);
